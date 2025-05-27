f82a1b831fb0caa19fe62f4ef50bed8d9e9b3951
briannjoroge <briannjoroge007@gmail.com>
2025-05-27 19:25:46 +0300

    added .js file for the project

---
+++ app.js
@@ -0,0 +1,28 @@
+const searchName = document.getElementById("search-name");
+const searchButton = document.querySelector(".search-btn");
+const outputTag = document.querySelector(".output");
+
+searchButton.addEventListener("click", async () => {
+  const searchValue = searchName.value;
+  if (searchValue === "") {
+    outputTag.innerHTML = "Please provide a name.";
+    return;
+  }
+
+  searchButton.textContent = "Loading....";
+  searchButton.setAttribute("disabled", true);
+  try {
+    const feedback = await fetch(
+      `https://api.nationalize.io/?name=${searchValue}`,
+    );
+    const feedbackData = await feedback.json();
+    outputTag.innerHTML = `<span>${feedbackData.name}</span> is from <span>${feedbackData.country[0].country_id}</span> with <span>${Math.floor(feedbackData.country[0].probability * 100)}</span>% certainity.`;
+  } catch (error) {
+    outputTag.textContent = "Name not defined..";
+  } finally {
+    searchButton.removeAttribute("disabled", false);
+    searchButton.innerHTML = `<i class="fa-brands fa-searchengin"></i>Search`;
+  }
+
+  // console.log(feedbackData)
+});
