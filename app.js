616c78565af9063734d59a9962bdacd7323424ba
briannjoroge <briannjoroge007@gmail.com>
2025-05-27 19:55:37 +0300

    converts the country code to full name

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+window.onload = function () {
+  document.getElementById("search-name").focus();
+};
+
 const searchName = document.getElementById("search-name");
 const searchButton = document.querySelector(".search-btn");
 const outputTag = document.querySelector(".output");
+
+const getCountryNames = new Intl.DisplayNames(["en"], { type: "region" });
 
 searchButton.addEventListener("click", async () => {
   const searchValue = searchName.value;
@@ -16,9 +22,9 @@ searchButton.addEventListener("click", async () => {
       `https://api.nationalize.io/?name=${searchValue}`,
     );
     const feedbackData = await feedback.json();
-    outputTag.innerHTML = `<span>${feedbackData.name}</span> is from <span>${feedbackData.country[0].country_id}</span> with <span>${Math.floor(feedbackData.country[0].probability * 100)}</span>% certainity.`;
+    outputTag.innerHTML = `<span>${feedbackData.name}</span> is from <span> ${getCountryNames.of(feedbackData.country[0].country_id)}</span> with <span>${Math.floor(feedbackData.country[0].probability * 100)}</span>% certainity.`;
   } catch (error) {
-    outputTag.textContent = "Name not defined..";
+    outputTag.textContent = "Name not defined or no country data found.";
   } finally {
     searchButton.removeAttribute("disabled", false);
     searchButton.innerHTML = `<i class="fa-brands fa-searchengin"></i>Search`;
